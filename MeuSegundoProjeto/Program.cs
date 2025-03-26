99c96018b8102be261681c0561c4c107e3b8d894
EnzoNeve5 <111204969+EnzoNeve5@users.noreply.github.com>
2025-03-26 20:30:43 -0300

    Add files via upload

---
+++ MeuSegundoProjeto/Program.cs
@@ -0,0 +1,21 @@
+﻿string nome;
+int num1, num2;
+Console.Write("Qual é o seu nome: ");
+nome = Console.ReadLine()!;
+Console.WriteLine("Olá, " + nome);
+
+Console.Write("Digite um número: ");
+num1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine(num1);
+
+Console.Write("Digite outro número: ");
+num2 = int.Parse(Console.ReadLine()!);
+Console.WriteLine(num2);
+
+Console.WriteLine();
+
+Console.WriteLine(num1 + " + " + num2 + " = " + (num1 + num2));
+Console.WriteLine(num1 + " - " + num2 + " = " + (num1 - num2));
+Console.WriteLine(num1 + " x " + num2 + " = " + (num1 * num2));
+
+Console.ReadLine();
