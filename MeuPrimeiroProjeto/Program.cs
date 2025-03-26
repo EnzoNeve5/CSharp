cd233869b341a9a2d1c9ac08e9ac5ed394451a5e
EnzoNeve5 <111204969+EnzoNeve5@users.noreply.github.com>
2025-03-26 14:37:47 -0300

    Add files via upload

---
+++ MeuPrimeiroProjeto/Program.cs
@@ -0,0 +1,17 @@
+﻿int idade = 20, dia = 26, mes = 3, ano = 2025;
+float altura = 1.72f, nota = 8.5f;
+double peso = 60.5;
+char inicial = 'A';
+Console.WriteLine("Olá, Mundo!");
+Console.WriteLine("Minha idade é: " + idade);
+Console.WriteLine("Minha altura é: " + altura);
+Console.WriteLine("Meu peso é: " + peso);
+Console.WriteLine("A primeira letra do alfabeto é: " + inicial);
+Console.WriteLine("Hoje é " + dia + "/0" + mes + "/" + ano);
+Console.WriteLine("Nota: " + nota);
+// Este é um comentário em linha
+/* 
+Este é um
+comentário
+em texto 
+*/
